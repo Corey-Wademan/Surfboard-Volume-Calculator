3227cc5303fa45a5d3adc146933c8d98a8176120
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2020-11-27 15:49:03 -0500

    Finished functions / styles setup

--- src/App.js
+++ src/App.js
@@ -3,30 +3,65 @@ import './App.css';
 import VolumeDisplay from './Components/VolumeDisplay';
 
 function App() {
-  const [weight, setWeight] = useState(null);
-  const [experience, setExperience] = useState(null);
-  const [volume, setVolume] = useState(null);
+  const [weight, setWeight] = useState();
+  const [experience, setExperience] = useState('beginner');
+  const [volume, setVolume] = useState();
 
-  //Functions
-
-  const calculateWeightRange = (input) => {
-
+  // records weight value
+  const handleWeight = (val) => {
+   setWeight(val.target.value);
+  }
+  // handles experience input
+  const handleExperience = (skillLevel) => {
+    setExperience(skillLevel.target.value);
   }
 
-  const calcFinalRange = () => {
+  // calculates volume range based off weight and experience levels - converted into kg from pounds
+  const calcTotal = () => {
+    if (experience === 'beginner') {
+      const value1 = (weight / 2.2) * .80;
+      const value2 = (weight / 2.2) * 1;
 
-  }
+      const total = `${value1.toFixed(2)} - ${value2.toFixed(2)} Liters`;
+      return total
+    } 
 
+    else if (experience === 'intermediate') {
+      const value1 = (weight / 2.2) * .44;
+      const value2 = (weight / 2.2) * .50;
+
+      const total = `${value1.toFixed(2)} - ${value2.toFixed(2)} Liters`;
+      return total
+    } 
+
+    else if (experience === 'advanced') {
+      const value1 = (weight / 2.2) * .38;
+      const value2 = (weight / 2.2) * .42;
+
+      const total = `${value1.toFixed(2)} - ${value2.toFixed(2)} Liters`;
+      return total
+    }
+  };
+
+
+    
+  
+
+  // sends return of final range as props onClick
   const handleClick = () => {
-
-    setVolume(calcFinalRange());
+    setVolume(calcTotal());
   }
 
 
   return (
-    <div id='wrapper'>
+    <div>
       <VolumeDisplay 
-
+        handleExperience={handleExperience}
+        handleClick={handleClick}
+        handleWeight={handleWeight}
+        weight={weight}
+        experience={experience}
+        total={volume}
       />
     </div>
   );
