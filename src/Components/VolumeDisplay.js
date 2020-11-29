f58fc3f84aefc98cb8c6dc5e8f8ecfd0810eb30c
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2020-11-28 23:53:15 -0500

    Add files via upload

--- src/Components/VolumeDisplay.js
+++ src/Components/VolumeDisplay.js
@@ -3,18 +3,26 @@ import React from 'react'
 const VolumeDisplay = (props) => {
 
 
+
+
     return (
         <div id='container'>
-            <input id='weight-slider' type='range' min='80' max='250' step='5' value={props.weight} onChange={props.handleWeight} />
+            <div id='sliderContainer'>
+                <span class='text' >{`${props.weight} lbs`}</span>
+                <input id='weightSlider'  type='range' min='80' max='250' step='1' value={props.weight} onChange={props.handleWeight} />
+            </div>
 
-            <label for='experience-level' >Skill Level:</label>
-            <select value={props.experience} onChange={props.handleExperience} name='experience-level' id='experience-level'>
-                <option id='option1' value='beginner'>Beginner</option>
-                <option id='option2' value='intermediate'>Intermediate</option>
-                <option id='option3' value='advanced'>Advanced</option>
-            </select>
+            <div id='label-container'>
+                <label for='experience-level' >Skill Level:</label>
+                <select value={props.experience} onChange={props.handleExperience} name='experience-level' id='experience-level'>
+                    <option id='option1' value='beginner'>Beginner</option>
+                    <option id='option2' value='intermediate'>Intermediate</option>
+                    <option id='option3' value='advanced'>Advanced</option>
+                </select>
+            </div>
+            
             <button onClick={props.handleClick}>Calculate Volume</button>
-            <h2>{props.total}</h2>
+            <h2 class='text' id='result'>{props.total}</h2>
         </div>
     )
 }
