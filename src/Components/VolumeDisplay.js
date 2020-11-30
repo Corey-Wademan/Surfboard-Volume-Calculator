87fae9a8025b135f0d54d4f6dde989605b7773ac
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2020-11-29 23:18:14 -0500

    Add files via upload

--- src/Components/VolumeDisplay.js
+++ src/Components/VolumeDisplay.js
@@ -7,22 +7,24 @@ const VolumeDisplay = (props) => {
 
     return (
         <div id='container'>
-            <div id='sliderContainer'>
-                <span class='text' >{`${props.weight} lbs`}</span>
-                <input id='weightSlider'  type='range' min='80' max='250' step='1' value={props.weight} onChange={props.handleWeight} />
-            </div>
+            <div id='display'>
+                <div id='sliderContainer'>
+                    <span class='text'>{`${props.weight} lbs`}</span>
+                    <input id='weightSlider'  type='range' min='80' max='250' step='1' value={props.weight} onChange={props.handleWeight} />
+                </div>
 
-            <div id='label-container'>
-                <label for='experience-level' >Skill Level:</label>
-                <select value={props.experience} onChange={props.handleExperience} name='experience-level' id='experience-level'>
-                    <option id='option1' value='beginner'>Beginner</option>
-                    <option id='option2' value='intermediate'>Intermediate</option>
-                    <option id='option3' value='advanced'>Advanced</option>
-                </select>
+                <div id='label-container'>
+                    <label for='experience-level' >Skill Level:</label>
+                    <select value={props.experience} onChange={props.handleExperience} name='experience-level' id='experience-level'>
+                        <option id='option1' value='beginner'>Beginner</option>
+                        <option id='option2' value='intermediate'>Intermediate</option>
+                        <option id='option3' value='advanced'>Advanced</option>
+                    </select>
+                </div>
+                
+                <button id='calculate-button' onClick={props.handleClick}>Calculate Volume</button>
+                <h2 id='result'>{props.total}</h2>
             </div>
-            
-            <button onClick={props.handleClick}>Calculate Volume</button>
-            <h2 class='text' id='result'>{props.total}</h2>
         </div>
     )
 }
